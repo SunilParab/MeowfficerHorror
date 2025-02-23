b03832012ca96c6a8f1a394a07e9dd5b0f945f98
Sunil Parab <sunilparab2023@gmail.com>
2025-02-23 16:25:03 -0500

    Made project and player controls

---
+++ Assets/CameraController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    float sensitivity = 5;
+    float verticalRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalRotation -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        transform.localEulerAngles = new Vector3(verticalRotation, 0, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //Movement Variables
+    [SerializeField]
+    Rigidbody rb;
+    [SerializeField]
+    GameObject model;
+    [SerializeField]
+    float moveAcceleration = 5;
+    [SerializeField]
+    float maxSpeed = 50;
+    [SerializeField]
+    float sensitivity = 5;
+
+    public int score = 0;
+    
+    //Input systems
+    InputAction moveAction;
+    InputAction interactAction;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        interactAction = InputSystem.actions.FindAction("Interact");
+    }
+
+    void Update()
+    {
+        //Rotate to mouse
+        float rotateHorizontal = Input.GetAxis("Mouse X");
+        //Turn player
+        transform.Rotate(transform.up * rotateHorizontal * sensitivity * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Movement
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        Vector3 moveValue = new Vector3(moveInput.x, 0, moveInput.y);
+
+        
+        rb.AddForce(transform.rotation*(moveAcceleration*moveValue),ForceMode.Force);
+        
+
+        if (rb.linearVelocity.magnitude > maxSpeed) {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
+ 
+    }
+
+
+    void OnTriggerEnter(Collider collision) {
+        GameObject other = collision.gameObject;
+        score += other.tag.Equals("Coin") ? 5 : -10;
+        Destroy(other);
+    }
+
+}
